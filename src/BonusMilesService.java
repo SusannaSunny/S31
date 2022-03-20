4c26ed8c07da9dd3246fdcdac413b1ebde1f835f
Susanna <susanna.demkina@gmail.com>
2022-03-20 16:46:55 +0300

    new file:   .gitignore 	new file:   src/BonusMilesService.java 	new file:   src/Main.java

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int countMilli;
+        int rubMilli = 20;
+
+        countMilli = cost / rubMilli;
+        return countMilli;
+
+    }
+}
+
